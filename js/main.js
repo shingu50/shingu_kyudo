4be3417a197909d9e710eac0f2a189480b46a285
shingu <bp1224011@gs.seito-fku.ed.jp>
2026-03-11 20:34:29 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -179,7 +179,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const contentDiv = document.createElement('div');
         contentDiv.className = 'message-content';
-        contentDiv.innerHTML = '<div class="dot"></div><div class="dot"></div><div class="dot"></div>';
+        contentDiv.innerHTML = `
+            <div class="kyudo-loader">
+                <div class="loader-arrow-fly"></div>
+                <div class="loader-target-kasumi"></div>
+            </div>
+        `;
 
         messageDiv.appendChild(contentDiv);
         chatHistory.appendChild(messageDiv);
